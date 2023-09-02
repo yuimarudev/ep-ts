5aa02a1e155dc0176d2e5fd931116dada34a6cd5
yuimarudev <yuimarudev@outlook.jp>
2023-09-02 21:44:49 +0900

    added `setPanicHandler`

--- src/functions/panic.ts
+++ src/functions/panic.ts
@@ -1,3 +1,32 @@
-export function panic<E extends Error>(error: E): never {
+export type panicHandler<E extends Error = Error> = (error: E) => never;
+
+const _fallback: panicHandler = (error) => {
   throw error;
+};
+
+const nodePanic: panicHandler = (error) => {
+  console.error(error);
+
+  process.exit(1);
+};
+
+const browserPanic: panicHandler = (error) => {
+  console.error(error);
+
+  while (true);
+};
+
+let panicHandler: panicHandler =
+  typeof console !== "undefined" && typeof console.error !== "undefined"
+    ? typeof process !== "undefined"
+      ? nodePanic
+      : browserPanic
+    : _fallback;
+
+export function setPanicHandler(handler: panicHandler) {
+  panicHandler = handler;
 }
+
+export const panic: panicHandler = (error) => {
+  panicHandler(error);
+};
