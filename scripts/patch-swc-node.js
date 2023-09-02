0c08ad98db4ed6789d83fa00a41ac494bb7e6476
yuimarudev <yuimarudev@outlook.jp>
2023-09-02 20:52:25 +0900

    use pnpm & pnpm patches

--- scripts/patch-swc-node.js
+++
@@ -1,9 +0,0 @@
-import { readFile, writeFile } from "node:fs/promises";
-
-const source = await readFile(
-  "node_modules/@swc-node/register/lib/register.js",
-  "utf-8"
-);
-const patched = source.replaceAll("            return sourcecode;", "");
-
-await writeFile("node_modules/@swc-node/register/lib/register.js", patched);
